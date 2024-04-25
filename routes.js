d986959423e2e8d0dbd0b32b00214cd9162a7894
ghufron akbar <alan.040219.gam@gmail.com>
2024-04-26 06:11:33 +0700

    Controller Admin

--- routes.js
+++ routes.js
@@ -1,39 +1,47 @@
 'use strict';
 
 module.exports = function(app)  {
-    var jsonku = require('./controller');
+    var apiAdmin = require('./controllers/admin')
 
-    app.route('/')
-    .get(jsonku.index);
 
-    app.route('/animals')
-    .get(jsonku.animals);
+    app.route('/v1/web/animals')
+    .get(apiAdmin.animal_controller.webanimalid);
 
-    app.route('/animals/data/:id')
-    .get(jsonku.animalsid);
+    app.route('/v1/web/animal/:id')
+    .get(apiAdmin.animal_controller.webanimalid);
 
-    app.route('/users')
-    .get(jsonku.users);
+    app.route('/v1/web/animal/edit/:id')
+    .put(apiAdmin.animal_controller.webanimaledit);
 
-    app.route('/users/data/:id')
-    .get(jsonku.usersid);
+    app.route('/v1/web/animal/delete/:id')
+    .delete(apiAdmin.animal_controller.webanimaldelete);
 
-    app.route('/request/data')
-    .get(jsonku.reqdata);
+    app.route('/v1/web/request/accounts')
+    .get(apiAdmin.request_account_controller.webrequestaccounts);
 
-    app.route('/request/data/id/:id')
-    .get(jsonku.reqdataid);
+    app.route('/v1/web/request/account/:id')
+    .get(apiAdmin.request_account_controller.webrequestaccountid);
 
-    app.route('/request/data/pending')
-    .get(jsonku.reqdatapend);
+    app.route('/v1/web/request/account/approve/:id')
+    .put(apiAdmin.request_account_controller.webapproverequestaccount);
 
-    app.route('/request/account')
-    .get(jsonku.reqacc);
+    app.route('/v1/web/request/datas')
+    .get(apiAdmin.request_data_controller.webrequestdatas);
 
-    app.route('/request/account/id/:id')
-    .get(jsonku.reqaccid);
+    app.route('/v1/web/request/data/:id')
+    .get(apiAdmin.request_data_controller.webrequestdataid);
 
-    app.route('/request/account/pending')
-    .get(jsonku.reqaccpend);
+    app.route('/v1/web/request/data/approve/:id')
+    .put(apiAdmin.request_data_controller.webapproverequestdata);
+
+    app.route('/v1/web/request/data/approve/send')
+    .post(apiAdmin.request_data_controller.websendrequestdata);
+   
+    app.route('/v1/web/history/request/data')
+    .get(apiAdmin.history_request_data_controller.webhistoryrequestdatas);
+
+    app.route('/v1/web/history/request/data/:id')
+    .get(apiAdmin.history_request_data_controller.webhistoryrequestdataid);
    
 }
+
