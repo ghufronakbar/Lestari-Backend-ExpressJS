def7ce4cf3dd119d48e45333909b64c60582a5d4
ghufron akbar <alan.040219.gam@gmail.com>
2024-04-26 16:47:36 +0700

    User Editable and History Controller

---
+++ controllers/user/index.js
@@ -0,0 +1,2 @@
+exports.editable_animal_controller = require('./editable_animal_controller')
+exports.history_animal_controller = require('./history_animal_controller')
--- routes.js
+++ routes.js
@@ -2,8 +2,10 @@
 
 module.exports = function(app)  {
     var apiAdmin = require('./controllers/admin')
+    var apiUser = require('./controllers/user')
 
 
+    //API ADMIN
     app.route('/v1/web/animals')
     .get(apiAdmin.animal_controller.webanimalid);
 
@@ -42,6 +44,32 @@ module.exports = function(app)  {
 
     app.route('/v1/web/history/request/data/:id')
     .get(apiAdmin.history_request_data_controller.webhistoryrequestdataid);
+
+
+
+    //API USER
+    app.route('/v1/mob/animals/editable/:token')
+    .get(apiUser.editable_animal_controller.mobeditableanimals);
+
+    app.route('/v1/mob/animal/editable/:token/:id_animal')
+    .get(apiUser.editable_animal_controller.mobeditableanimalid);
    
+    app.route('/v1/mob/animal/add')
+    .post(apiUser.editable_animal_controller.mobanimalpost);
+
+    app.route('/v1/mob/animal/editable/edit/:token/:id_animal')
+    .put(apiUser.editable_animal_controller.mobediteditableanimal);
+
+    app.route('/v1/mob/animal/editable/edit/image/:token/:id_animal')
+    .put(apiUser.editable_animal_controller.mobediteditableanimalimage);
+
+
+    app.route('/v1/mob/animals/history/:token')
+    .get(apiUser.history_animal_controller.mobhistoryanimals);
+
+    app.route('/v1/mob/animal/history/:token/:id_animal')
+    .get(apiUser.history_animal_controller.mobhistoryanimalid);
+    
+
 }
 
